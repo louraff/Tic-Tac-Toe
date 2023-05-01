178ba2a6b1f56862c45b0362f65a478caf78b747
Louise Raffray <raffraylouise@gmail.com>
2023-05-01 12:31:12 +0100

    updated winner function

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ function cellClick(evt) {
     
     colArr[rowIdx] = turn;
     turn *= -1;
-    winner = getWinner(board, turn);
+    winner = getWinner(colIdx, rowIdx);
     render();
 };
 
@@ -69,13 +69,91 @@ function resetGame(evt) {
 
 };
 
+function getWinner(colIdx, rowIdx) {
+    let verticalWin = checkVerticalWin(colIdx, rowIdx);
+    let horizontalWin = checkHorizontalWin(colIdx, rowIdx);
+    let diagonalWinNESW = checkDiagonalWinNESW(colIdx, rowIdx);
+    let diagonalWinNWSE = checkDiagonalWinNWSE(colIdx, rowIdx);
+    
+    if (verticalWin || horizontalWin || diagonalWinNESW || diagonalWinNWSE) {
+        return winner;
+    } else {
+        checkTie(colIdx, rowIdx);
+        if (true) {
+            winner = 'T'
+        } else {
+            winner = null;
+        }
+    };
+};
+
+function checkTie(colIdx, rowIdx) {
+    if (!winner) {
+        for(colArr of board) {
+            for(rowArr of colArr) {
+                if (board[colIdx][rowIdx] === 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } 
+    } else {
+        winner = null;
+    };
+};
+
+function checkDiagonalWinNWSE(colIdx, rowIdx) { // colIdx, rowIdx are where the last move was made
+	const adjCountNW = countAdjacent(colIdx, rowIdx, -1, 1);
+	const adjCountSE = countAdjacent(colIdx, rowIdx, 1, -1);
+	return (adjCountNW + adjCountSE) >= 2 ? board[colIdx][rowIdx] : null;
+};
+
+function checkDiagonalWinNESW(colIdx, rowIdx) { 
+	const adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1);
+	const adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1);
+	return (adjCountNE + adjCountSW) >= 2 ? board[colIdx][rowIdx] : null;
+};
+
+
+function checkHorizontalWin(colIdx, rowIdx) { 
+	const adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0);
+	const adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0);
+	return (adjCountLeft + adjCountRight) >= 2 ? board[colIdx][rowIdx] : null;
+};
+
+function checkVerticalWin(colIdx, rowIdx) { 
+	return countAdjacent(colIdx, rowIdx, 0, -1) === 2 ? board[colIdx][rowIdx] : null;
+};
+
+function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    //Shortcut variable to the player value
+        const player = board[colIdx][rowIdx];
+    //track count of adjacent cells with the same player value
+        let count = 0;
+    //initialise the new coordinates
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+        while (
+    // Ensure the colIdx is within bounds of the board array
+        board[colIdx] !== undefined && 
+        board[colIdx][rowIdx] !== undefined &&
+        board[colIdx][rowIdx] === player
+    ) {
+        count++;
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+        }
+        return count;
+    };
+
 function renderBoard () {
     board.forEach(function(colArr, colIdx){
         colArr.forEach(function(cellVal, rowIdx){
             const cellId = `c${colIdx}r${rowIdx}`;
             const cellEl = document.getElementById(cellId);
-            cellEl.style.backgroundColor = COLORS[cellVal];
-console.log(cellEl);
+            cellEl.style.color = COLORS[cellVal];
+            cellEl.innerText = `${PLAYER[cellVal]}`;
         })
     })
 };
